3930ddebd27766dd4877c2be529f3c73c0eb26dd
Yuviar <yuviarnuzu21@gmail.com>
2023-09-14 11:37:59 +0700

    pee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
         <div class="mt-4 text-center">
             <h3 class="text-2xl font-bold">Our Menu</h3>
             <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-                TODAY'S SPECIALITY</h2>
+                TODAY'S SPECIALITY</h2> 
         </div>
         <div class="container w-full px-5 py-6 mx-auto">
             @if ($special)
